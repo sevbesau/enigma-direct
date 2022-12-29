3aea7ea06fe58a6387fc20c25371d620dbc66154
seppe van besauw <seppe.vanbesauw@icapps.com>
2022-12-29 16:08:58 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+
+const redirects = require('./redirects.json');
+console.log(redirects);
+
+require('dotenv').config();
+
+const app = express();
+
+app.set('views', path.join(__dirname, '..', 'views'));
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.use(morgan('tiny'));
+
+app.get('*', (req, res) => {
+    const slug = req.url.replace(/^\//, '');
+    const url = redirects[slug];
+    res.render('index', { url });
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`[express] app listening on port ${process.env.PORT}`);
+});
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './views/*.{pug, html}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
